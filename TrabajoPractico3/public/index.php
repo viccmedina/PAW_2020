032129c4c6efd3404ae256ddbb0b11e25a554bf6
Agustin <NormandAgustin@gmail.com>
2020-10-26 17:48:48 -0300

    Agrego monolog

--- TrabajoPractico3/public/index.php
+++ TrabajoPractico3/public/index.php
@@ -4,22 +4,34 @@
 
     use Paw\App\Controllers\PageController;
     use Paw\App\Controllers\ErrorController;
-
-    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    use Monolog\Logger;
+    use Monolog\Handler\StreamHandler;
 
     $controller = new PageController;
 
+    $log = new Logger('mvc-app');
+    $log->pushHandler(new StreamHandler(__DIR__. '/../logs/app.log', Logger::DEBUG));
+
+    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+    $log->info("Petición a: {$path}");
+
     if($path == '/'){
         $controller->index();
+        $log->info("Respuesta exitosa: 200");
     }else if ($path == '/services'){
         $controller->services();
+        $log->info("Respuesta exitosa: 200");
     }else if ($path == '/about'){
         $controller->about();
+        $log->info("Respuesta exitosa: 200");
     }else if ($path == '/contacts'){
         $controller->about();
+        $log->info("Respuesta exitosa: 200");
     }
     else{
         $constroller = new ErrorController;
         $constroller->notFound();
+        $log->info("Path not found: 404");
     }
 ?>
