f159059a480e89561614666273134f94dcd58623
Agustin <NormandAgustin@gmail.com>
2020-10-26 14:52:27 -0300

    Agrego el error 404, mejoras en routeo

--- TrabajoPractico3/public/index.php
+++ TrabajoPractico3/public/index.php
@@ -24,11 +24,11 @@
 
     $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-    $whoops = new \Whoops\Run;
-    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-    $whoops->register();
+    #$whoops = new \Whoops\Run;
+    #$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    #$whoops->register(); 
 
-    throw new \Exception("Mensaje de error para el desarrollador");
+    #throw new \Exception("Mensaje de error para el desarrollador");
 
     if($path == '/'){
         $titulo = htmlspecialchars($_GET['nombre'] ?? "PAW");
@@ -37,9 +37,19 @@
         $title = "Servicios";
         $main = "Página de servicios";
         require __DIR__ . '/../src/services.view.php';
-    }else{
+    }else if ($path == '/about'){
+        $title = "Sobre Nosotros";
+        $main = "Página Institucional";
+        require __DIR__ . '/../src/services.view.php';
+    }else if ($path == '/contacts'){
+        $title = "Contacto";
+        $main = "Formas de contacto";
+        require __DIR__ . '/../src/contact.view.php';
+    }
+    else{
         //Debería generarse un codigo 404
-        echo "Page Not Found";
+        http_response_code(404);
+        require __DIR__ . '/../src/not-found.view.php';
     }
 
   
---
+++ TrabajoPractico3/src/not-found.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page not found</title>
+</head>
+<body>
+    <header>
+        <h1>Page not found</h1>
+        <nav>
+            <ul>
+                <?php foreach($menu as $item) : ?>
+                    <li><a href="<?= $item["href"]?>"><?= $item["name"]?></a></li>
+                <?php endforeach ;?>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
