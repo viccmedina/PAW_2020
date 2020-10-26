01ad875087b3380a4dd6142bfb227e4bcf635181
Agustin <NormandAgustin@gmail.com>
2020-10-26 13:09:02 -0300

    Agrego routeo y vista de servicios

--- TrabajoPractico3/public/index.php
+++ TrabajoPractico3/public/index.php
@@ -10,16 +10,28 @@
             "name" => "Quienes Somos"
         ],
         [
-            "href" => "/servicios",
+            "href" => "/services",
             "name" => "Servicios"
         ],
         [
-            "href" => "/contactos",
+            "href" => "/contacts",
             "name" => "Contactos"
         ],
     ];
 
-    $titulo = htmlspecialchars($_GET['nombre'] ?? "PAW");
+    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-    require __DIR__ . '/../src/index.view.php'; 
+    if($path == '/'){
+        $titulo = htmlspecialchars($_GET['nombre'] ?? "PAW");
+        require __DIR__ . '/../src/index.view.php';
+    }else if ($path == '/services'){
+        $title = "Servicios";
+        $main = "Página de servicios";
+        require __DIR__ . '/../src/services.view.php';
+    }else{
+        //Debería generarse un codigo 404
+        echo "Page Not Found";
+    }
+
+  
 ?>
---
+++ TrabajoPractico3/src/services.view.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primer documento PAW</title>
+</head>
+<body>
+    <header>
+        <h1><?= $title ?></h1>
+        <nav>
+            <ul>
+                <?php foreach($menu as $item) : ?>
+                    <li><a href="<?= $item["href"]?>"><?= $item["name"]?></a></li>
+                <?php endforeach ;?>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <?= $main ?>
+    </main>
+</body>
+</html>
