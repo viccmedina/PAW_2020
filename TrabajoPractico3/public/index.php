6ba11bd74cb24ee814a910b9feee284d49422520
beetlejuice96 <dilanbernbae043@gmail.com>
2020-10-26 11:02:18 -0300

    se agrega un script bootstrap.php

--- TrabajoPractico3/public/index.php
+++ TrabajoPractico3/public/index.php
@@ -5,23 +5,10 @@
 //autoload es un mecanismo de php que me dice que cargue de forma automatica todos los objetos y clases que haya en este proyecto.
 
 //
-require __DIR__. "/../vendor/autoload.php";
+require __DIR__. "/../src/bootstrap.php";
 
 use Paw\App\Controllers\ErrorController;
 use Paw\App\Controllers\PageController;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
-$log = new Logger('mvc-app');//ese nombre es un identificador ya que monolog nos permite tener varios logger para difertes cosas.
-$log->pushHandler(new StreamHandler(__DIR__.'/../logs/app.log',Logger::DEBUG));
-
-
-$whoops = new \Whoops\Run;
-$whoops ->pushHandler(new \Whoops\Handler\PrettyPageHandler);//esto significa que puedo generar mi propio handler.
-
-$whoops->register();// con esto le estoy indicando que sobre escrib las funciones de errores de php y que pase el a manejar los errores
-//throw new \Exception("Mensaje de error para desarrollador");
-
 
 /*La variable $_SERVER me da mucha data sobre lo que llega del lado del cliente.*/
 /*echo "<pre>";
