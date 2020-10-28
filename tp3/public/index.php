3fe3cbff344a6ed40bf165eb11bd0e409c8bf731
iarzaesteban <iarzaesteban94@gmail.com>
2020-10-28 00:43:03 -0300

    subo logs

--- tp3/public/index.php
+++ tp3/public/index.php
@@ -3,6 +3,12 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Paw\App\Controllers\PageController;
+use Monolog\Logger;
+
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('mvc-app');
+$log ->pushHandler(new StreamHandler(__DIR__ . '/../log/app.log', Logger::DEBUG));
 
 $whoops = new \Whoops\Run;
 
@@ -13,17 +19,21 @@ $controller = new PageController;
 
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-
+$log -> info("Peticion a:  {$path}");
 
 
 if ($path == '/'){
 	$controller-> index();
+	$log-> info("Respuesta existosa 200OK");
 }
 if ($path == '/home'){
 	$controller-> index();
+	$log-> info("Respuesta existosa 200OK");
 }else if ($path == '/turnos'){
 	$controller -> turnos();
+	$log-> info("Respuesta existosa 200OK");
 }else {
 	$controller -> notFoundPage();
+	$log-> info("Path not found 404");
 }
 
