3e9e0778c9bf9c2be7dfec25e75695006d32eadb
Victoria Medina <vmedina@msa.com.ar>
2020-10-29 22:20:53 -0300

    logs con monologs!

--- PAW/public/index.php
+++ PAW/public/index.php
@@ -6,6 +6,11 @@ use Paw\App\Controllers\PageController;
 
 use Paw\App\Controllers\ErrorController;
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('mvc-app');
+$log->pushHandler(new StreamHandler(__DIR__ . '/../logs/app.log', Logger::DEBUG));
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
@@ -35,27 +40,32 @@ $controller = new PageController;
 */
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-
+$log->info("Petición a: {$path}");
 if ($path == '/') {
 
 	$controller->index();
+	$log->info('Respuesta Exitosa: 200');
 
 } else if ($path == '/services') {
-	
+
 	$controller->services();
+	$log->info('Respuesta Exitosa: 200');
 
 }  else if ($path == '/about') {
 
 	$controller->about();
+	$log->info('Respuesta Exitosa: 200');
 
 } else if ($path == '/contact') {
 
 	$controller->contact();
+	$log->info('Respuesta Exitosa: 200');
 
 } else {
 	// deberíamos generar una vista para el 404 not found
 	$controller = new ErrorController;
 	$controller->notFound();
+	$log->info('Path Not Found: 404');
 }
 
 ?>
